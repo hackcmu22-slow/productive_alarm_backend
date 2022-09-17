ca0c5d1df78f5f3bf2c2b6a8191202bf23b1deff
Lhwhatever <llinhong.2301+github@gmail.com>
2022-09-17 17:45:49 -0400

    Implement communication with frontend

--- yolov5/app.py
+++ yolov5/app.py
@@ -5,7 +5,8 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
-from flask import Flask, Response
+from flask import Flask, Response, request
+import base64
 
 # Flask App
 app = Flask(__name__)
@@ -15,21 +16,26 @@ cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 # GENERATING NORMAL VIDEO FEED FRAMES
-def gen_video_frames():  
+def gen_video_frames():
     while True:
         success, frame = cam.read()  # read the camera frame
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
+            ret, buffer = cv2.imencode(".jpg", frame)
             frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            yield (
+                b"--frame\r\n" b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+            )  # concat frame one by one and show result
+
 
 # Routing normal video feed
 @app.route("/")
 def video_feed():
-    return Response(gen_video_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        gen_video_frames(), mimetype="multipart/x-mixed-replace; boundary=frame"
+    )
+
 
 # STREAMING WITH YOLO (COMMENT OUT EVERYTHING BELOW THIS TO RUN WITHOUT YOLO)
 
@@ -41,7 +47,7 @@ from pathlib import Path
 import torch
 
 # Loading the model
-model = torch.hub.load('.', 'yolov5s', source='local', skip_validation=True)
+model = torch.hub.load(".", "yolov5s", source="local", skip_validation=True)
 model.classes = [0, 79]
 
 # Defining booleans
@@ -55,14 +61,14 @@ def gather_yolo(contains_person):
         try:
             results = model(img)
             results.render()
-            _, frame = cv2.imencode('.jpg', results.imgs[0])
-            predictions = results.pandas().xyxy[0] #Printing the predictions
+            _, frame = cv2.imencode(".jpg", results.imgs[0])
+            predictions = results.pandas().xyxy[0]  # Printing the predictions
             print(predictions["class"][0])
             try:
                 # Person Class: 0, Toothbrush == 79
-                if (predictions["class"][0] == 0 and predictions["class"][1] == 79):
-                    contains_person_and_toothbrush = True 
-                if contains_person_and_toothbrush == True: 
+                if predictions["class"][0] == 0 and predictions["class"][1] == 79:
+                    contains_person_and_toothbrush = True
+                if contains_person_and_toothbrush == True:
                     # Replace print("Wow!") to post a request to the success page.
                     print("WOW!")
             except:
@@ -72,9 +78,39 @@ def gather_yolo(contains_person):
             # Returning if a human and a toothbrush are detected.
         except Exception as e:
             print(e)
-            _, frame = cv2.imencode('.jpg', img)
-        yield (b'--frame\r\nContent-Type: imageviewer/jpeg\r\n\r\n' + frame.tobytes() + b'\r\n')
+            _, frame = cv2.imencode(".jpg", img)
+        yield (
+            b"--frame\r\nContent-Type: imageviewer/jpeg\r\n\r\n"
+            + frame.tobytes()
+            + b"\r\n"
+        )
+
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = request.get_json()["imgData"]
+    nparr = np.frombuffer(base64.b64decode(data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    img = img.transpose([1, 0, 2])
+    results = model(img)
+    print(results)
+    try:
+        predictions = results.pandas().xyxy[0]  # Printing the predictions
+        print("predictoins:", predictions["class"][0])
+        # Person Class: 0, Toothbrush == 79
+        if predictions["class"][0] == 0:
+            print("Found")
+            return {"result": True}
+    except Exception as e:
+        pass
+
+    print("No result")
+    return {"result": False}
+
 
 @app.route("/yolofeed")
 def yolo_mjpeg():
-    return Response(gather_yolo(contains_person), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(
+        gather_yolo(contains_person),
+        mimetype="multipart/x-mixed-replace; boundary=frame",
+    )
